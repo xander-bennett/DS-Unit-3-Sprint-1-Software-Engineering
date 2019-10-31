ecbacd1034d1a53dd03d6ccf62274a7e787bd4d0
xander-bennett <xander-bennett@users.noreply.github.com>
2019-10-31 09:40:04 -0700

    built sqrt.py. starting test file

---
+++ module4-software-testing-documentation-and-licensing/sqrt.py
@@ -0,0 +1,21 @@
+"""Bunch of functions that do square root. This is to show how to unit test"""
+
+# All the below functions are different methods for returning the square root.
+# We'll unit test to see how to identify what's failing/passing the test
+
+def lazy_swrt(x):
+    return x**0.5
+
+def builtin_sqrt(x):
+    from math import sqrt
+    return sqrt(x):
+
+def newton_sqrt(x):
+    """Uses the Newton method to return square root"""
+    val = x
+    while True:
+        last = val
+        val = (val + x /val) * 0.5
+        if abs(val - last) < 1e-9:
+            break
+    return val
